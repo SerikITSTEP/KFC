fd1f50452099c5250919156e64878f1d5acee946
Ковтун Александр <>
2016-02-22 20:27:16 +0600

    Create Models

---
+++ KFC/models/Positions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLibrary
+{
+  public  class Positions
+    {
+      [Key]
+      long id_position { get; set; }
+      string position { get; set; }
+    }
+}
---
+++ KFC/models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DatabaseLibrary
+{
+  public  class Users
+    {
+      [Key]
+      long id_user{get; set;}
+      string user { set; get; }
+      string password { set; get; }
+    }
+}
