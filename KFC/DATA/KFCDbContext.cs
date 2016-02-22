2d257d6fc91d994242142b63846566c0bcd528d2
Аманкельды Жалгас <>
2016-02-22 20:21:17 +0600

    Create DbContext

---
+++ KFC/DATA/KFCDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLibrary
+{
+    public class KFCDbContext : DbContext
+    {
+        public KFCDbContext()
+            : base(@"Server=1203_02PC\SQLEXPRESS120302;Database=KFCDbContext;Trusted_Connection=True;")
+        {
+        }
+
+        //
+
+    }
+}
